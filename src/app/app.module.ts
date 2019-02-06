bbbb0e50cf8d700385c09938921480885a714282
Bastien <bastien.nicoud@gmail.com>
2019-02-06 09:15:11 +0100

    Tests with menus and toolbar.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,33 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {
-  MatSidenav,
-  MatSidenavContainer,
-  MatSidenavContent,
-  MatToolbar,
-  MatIcon,
-  MatNavList
+  MatGridListModule,
+  MatCardModule,
+  MatMenuModule,
+  MatIconModule,
+  MatButtonModule,
+  MatSidenavModule,
+  MatToolbarModule
 } from '@angular/material';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LayoutModule } from '@angular/cdk/layout';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    MatSidenav,
     BrowserAnimationsModule,
-    MatSidenavContainer,
-    MatSidenavContent,
-    MatIcon,
-    MatToolbar,
-    MatNavList
+    MatToolbarModule,
+    MatSidenavModule,
+    MatGridListModule,
+    MatCardModule,
+    MatMenuModule,
+    MatIconModule,
+    MatButtonModule,
+    LayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
