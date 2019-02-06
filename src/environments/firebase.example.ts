4e1a695fff13ef18d23afc1bacda012bc57075e2
Bastien <bastien.nicoud@gmail.com>
2019-02-06 19:22:28 +0100

    Add dashboard module base component.

---
+++ src/environments/firebase.example.ts
@@ -0,0 +1,11 @@
+/**
+ * Copy this file and rename it to `firebase.ts` and add our keys.
+ */
+export const firebase = {
+  apiKey: '<your-key>',
+  authDomain: '<your-project-authdomain>',
+  databaseURL: '<your-database-URL>',
+  projectId: '<your-project-id>',
+  storageBucket: '<your-storage-bucket>',
+  messagingSenderId: '<your-messaging-sender-id>'
+};
