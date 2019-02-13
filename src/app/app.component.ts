77c776fd596ff4d46c90d2659ca0890b436e16a0
bastien <bastien.nicoud@gmail.com>
2019-02-13 11:36:57 +0100

    Add home page layout.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { NzMessageService } from 'ng-zorro-antd';
 
 @Component({
   selector: 'app-root',
@@ -7,12 +6,4 @@ import { NzMessageService } from 'ng-zorro-antd';
   styleUrls: ['./app.component.less']
 })
 export class AppComponent {
-  isCollapsed = false;
-  isReverseArrow = false;
-  width = 240;
-
-  constructor(private message: NzMessageService) {}
-  submitForm() {
-    this.message.success('Youpii bravo !');
-  }
 }
