542d000fb83dbf400a3a20856c40f2f8fa6087cf
bastien <bastien.nicoud@gmail.com>
2019-02-20 15:47:36 +0100

    Call the createAutomaton function when the form is valid.

--- src/app/views/dashboard/automatons/automatons-create/automatons-create.component.ts
+++ src/app/views/dashboard/automatons/automatons-create/automatons-create.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { NzMessageService } from 'ng-zorro-antd';
+import { AngularFireFunctions } from '@angular/fire/functions';
 
 @Component({
   selector: 'app-automatons-create',
@@ -9,22 +11,37 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class AutomatonsCreateComponent implements OnInit {
 
   form: FormGroup;
+  createAutomaton;
 
-  constructor(private fb: FormBuilder) { }
+  constructor(
+    private fb: FormBuilder,
+    private message: NzMessageService,
+    private functions: AngularFireFunctions
+  ) {
+    // Get a reference to firebase function
+    this.createAutomaton = functions.httpsCallable('create_automaton');
+  }
 
   ngOnInit() {
+    // Register reactive form
     this.form = this.fb.group({
-      name: [null, [Validators.minLength(2), Validators.maxLength(60), Validators.required]],
-      description: [null, [Validators.max(400)]]
+      name: [null, [
+        Validators.minLength(2),
+        Validators.maxLength(60),
+        Validators.required
+      ]],
+      description: [null, [
+        Validators.max(400)
+      ]]
     });
   }
 
   submitForm(): void {
-    for (const i in this.form.controls) {
-      this.form.controls[ i ].markAsDirty();
-      this.form.controls[ i ].updateValueAndValidity();
-    }
-    console.log(this.form);
+    this.createAutomaton(this.form.getRawValue()).subscribe(data => {
+      console.log(data);
+    }, err => {
+      console.error(err);
+    });
   }
 
 }
