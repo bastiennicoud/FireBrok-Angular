bcdf0da799f74d91b22f63e299376340e5b9314c
bastien <bastien.nicoud@gmail.com>
2019-02-20 18:36:11 +0100

    Get the snapshot reference of automatons, allows to extract the id of each elements.

--- src/app/views/dashboard/automatons/automatons-home/automatons-home.component.ts
+++ src/app/views/dashboard/automatons/automatons-home/automatons-home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { AngularFirestore } from '@angular/fire/firestore';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-automatons-home',
@@ -10,13 +11,21 @@ import { AngularFirestore } from '@angular/fire/firestore';
 export class AutomatonsHomeComponent implements OnInit {
 
   loading = false;
-  automatons: Observable<any>;
+  automatons$: Observable<any>;
 
   constructor(private firestore: AngularFirestore) {
-    this.automatons = firestore.collection(
+    this.automatons$ = firestore.collection(
       'automatons',
-      ref => ref.where('connected', '==', false)
-    ).valueChanges();
+      ref => ref.where('connected', '==', true)
+    ).snapshotChanges().pipe(
+      map(actions => actions.map(a => {
+        return {
+          id: a.payload.doc.id,
+          ...a.payload.doc.data()
+        };
+      }))
+    );
+    this.automatons$.subscribe(d => console.log(d));
   }
 
   ngOnInit() {
