8124864a60cf30b367778e8c99533aef59b222db
bastien <bastien.nicoud@gmail.com>
2019-02-21 18:55:30 +0100

    Add signIn, signOut, signUp methods to auth service.

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { Observable } from 'rxjs';
 import { User } from 'firebase';
+import { Router } from '@angular/router';
 
 /**
  * This service regroups all firebase authentication logic
@@ -17,7 +18,10 @@ export class AuthService {
   /** Store authenticated user observable */
   private user: Observable<User>;
 
-  constructor(private afAuth: AngularFireAuth) {
+  constructor(
+    private afAuth: AngularFireAuth,
+    private router: Router
+  ) {
     // Get user observable
     this.user = afAuth.user;
     // Register an observer on the auth state
@@ -38,5 +42,40 @@ export class AuthService {
    */
   get authenticatedUser(): Observable<User> {
     return this.user;
+  }
+
+  /**
+   * Sign up new user
+   */
+  signUp(email: string, password: string) {
+    return this.afAuth.auth.createUserWithEmailAndPassword(email, password)
+      .then(() => this.updateUserDatas())
+      .catch(error => console.error(error));
+  }
+
+  /**
+   * Sign in user
+   */
+  signIn(email: string, password: string) {
+    return this.afAuth.auth.signInWithEmailAndPassword(email, password)
+      .then(() => this.updateUserDatas())
+      .catch(error => console.error(error));
+  }
+
+  /**
+   * Sign out user and redirect to homepage
+   */
+  signOut(): void {
+    this.afAuth.auth.signOut()
+      .then(() => this.router.navigate(['/']))
+      .catch(error => console.error(error));
+  }
+
+  /**
+   * Update user datas
+   */
+  private updateUserDatas(): void {
+    // Update user firestore representation
+    console.log('Update firestore user representation');
   }
 }
