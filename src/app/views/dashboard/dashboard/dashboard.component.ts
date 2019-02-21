168ad7ca6c6b9b7d2deedd4344a1efed107daae0
bastien <bastien.nicoud@gmail.com>
2019-02-21 20:54:18 +0100

    Clear dashboard component unused members.

--- src/app/views/dashboard/dashboard/dashboard.component.ts
+++ src/app/views/dashboard/dashboard/dashboard.component.ts
@@ -1,8 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { NzMessageService } from 'ng-zorro-antd';
-import { AngularFirestore } from '@angular/fire/firestore';
-import { Observable } from 'rxjs';
-import { AngularFireFunctions } from '@angular/fire/functions';
+import { AuthService } from '../../../services/auth.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -15,32 +12,10 @@ export class DashboardComponent implements OnInit {
   isReverseArrow = false;
   width = 240;
 
-  topic: Observable<any>;
-  newAutomaton;
-
   constructor(
-    private message: NzMessageService,
-    private firestore: AngularFirestore,
-    private functions: AngularFireFunctions
-  ) {
-    // Get datas as observable from firestore
-    this.topic = firestore
-      .collection('automatons/raspberry/topics')
-      .valueChanges();
-    // Get reference to the new automatons function
-    this.newAutomaton = functions.httpsCallable('create_automaton');
-  }
-  submitForm() {
-    this.message.success('Creation d\'un automate');
-    let datas = this.newAutomaton({ name: 'testAutomaton' });
-    datas.subscribe(r => {
-      console.log(r);
-    }, e => {
-      console.error('Function error !', e);
-    });
-  }
+    private auth: AuthService
+  ) { }
 
-  ngOnInit() {
-  }
+  ngOnInit() { }
 
 }
