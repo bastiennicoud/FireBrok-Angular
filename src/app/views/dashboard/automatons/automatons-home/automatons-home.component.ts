c5c8fa96ac45a68740fcece6ddca4fc32c0f7861
bastien <bastien.nicoud@gmail.com>
2019-03-12 10:09:42 +0100

    Add a list of all user automatons on the automatons home page.

--- src/app/views/dashboard/automatons/automatons-home/automatons-home.component.ts
+++ src/app/views/dashboard/automatons/automatons-home/automatons-home.component.ts
@@ -12,9 +12,24 @@ export class AutomatonsHomeComponent implements OnInit {
 
   loading = false;
   automatons$: Observable<any>;
+  connectedAutomatons$: Observable<any>;
 
-  constructor(private firestore: AngularFirestore) {
+  constructor(
+    private firestore: AngularFirestore
+  ) {
+    // Get the observable for automatons
     this.automatons$ = firestore.collection(
+      'automatons'
+    ).snapshotChanges().pipe(
+      map(actions => actions.map(a => {
+        return {
+          id: a.payload.doc.id,
+          ...a.payload.doc.data()
+        };
+      }))
+    );
+    // Get the observable for connected automatons
+    this.connectedAutomatons$ = firestore.collection(
       'automatons',
       ref => ref.where('connected', '==', true)
     ).snapshotChanges().pipe(
