cbb7cab2c2a19afff518f34a8f5a035e059b356d
bastien <bastien.nicoud@gmail.com>
2019-03-12 10:08:59 +0100

    Add modal to see automaton authentication informations.

--- src/app/views/dashboard/automatons/automaton-details/automaton-details.component.ts
+++ src/app/views/dashboard/automatons/automaton-details/automaton-details.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router, ParamMap } from '@angular/router';
-import { switchMap } from 'rxjs/operators';
+import { map, switchMap } from 'rxjs/operators';
 import { Observable } from 'rxjs';
 import { AngularFirestore } from '@angular/fire/firestore';
 
@@ -11,6 +11,7 @@ import { AngularFirestore } from '@angular/fire/firestore';
 })
 export class AutomatonDetailsComponent implements OnInit {
 
+  isModalVisible = false;
   automaton$: Observable<any>;
   topics$: Observable<any>;
 
@@ -24,13 +25,24 @@ export class AutomatonDetailsComponent implements OnInit {
     // map the route parameters to get corresponding document
     this.automaton$ = this.route.paramMap.pipe(
       switchMap((params: ParamMap) => {
-        return this.firestore.doc(`/automatons/${params.get('id')}`).valueChanges();
+        return this.firestore.doc(`/automatons/${params.get('id')}`).valueChanges().pipe(
+          map(e => {
+            return { id: params.get('id'), ...e };
+          })
+        );
       })
     );
     // Map automaton topics
     this.topics$ = this.route.paramMap.pipe(
       switchMap((params: ParamMap) => {
-        return this.firestore.collection(`/automatons/${params.get('id')}/topics`).valueChanges();
+        return this.firestore.collection(`/automatons/${params.get('id')}/topics`).snapshotChanges().pipe(
+          map(actions => actions.map(a => {
+            return {
+              id: a.payload.doc.id,
+              ...a.payload.doc.data()
+            };
+          }))
+        );
       })
     );
   }
