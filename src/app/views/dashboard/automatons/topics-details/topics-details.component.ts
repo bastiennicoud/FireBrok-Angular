1ddf6d22e8b50298b0084662d594d7fb0f195b77
bastien <bastien.nicoud@gmail.com>
2019-03-13 10:28:23 +0100

    Add new view for topic details.

---
+++ src/app/views/dashboard/automatons/topics-details/topics-details.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-topics-details',
+  templateUrl: './topics-details.component.html',
+  styleUrls: ['./topics-details.component.less']
+})
+export class TopicsDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
