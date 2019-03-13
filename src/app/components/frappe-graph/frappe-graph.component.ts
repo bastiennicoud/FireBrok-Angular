9884577b863b29daf4a5a98cc7d117e794ef2665
bastien <bastien.nicoud@gmail.com>
2019-03-13 10:29:04 +0100

    Trasfer chart generation from directive to frappe-graph component.

--- src/app/components/frappe-graph/frappe-graph.component.ts
+++ src/app/components/frappe-graph/frappe-graph.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ViewChild } from '@angular/core';
+import { Chart } from 'frappe-charts';
+import { createViewChild } from '@angular/compiler/src/core';
 
 @Component({
   selector: 'app-frappe-graph',
@@ -7,9 +9,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FrappeGraphComponent implements OnInit {
 
-  constructor() { }
+  // Input decorator specifies props injectable in the directive via html attributes
+  @Input() type: string;
+  @Input() height: number;
+  @Input() data: any;
+
+  @ViewChild('frappe_chart') chartBox;
+
+  chart: Chart;
+
+  constructor() {
+    // Initialise default values if not specified into directive attributes
+    this.type = this.type || 'bar';
+    this.height = this.height || 250;
+  }
 
   ngOnInit() {
+    // Generate the frappe chart on init
+    this.chart = new Chart(
+      this.chartBox.nativeElement,
+      {
+        data: this.data,
+        type: this.type,
+        height: this.height
+      });
   }
 
 }
