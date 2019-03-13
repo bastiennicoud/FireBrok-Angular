0608912614a4ceeee69a427df488e8479d5fd60b
bastien <bastien.nicoud@gmail.com>
2019-03-13 08:41:31 +0100

    Add a specific directive to generate frappe charts.

--- src/app/directives/frappe.directive.ts
+++ src/app/directives/frappe.directive.ts
@@ -5,14 +5,16 @@ import { Chart } from 'frappe-charts';
  * Directive responsible to generate the frappe chart on targeted element
  */
 @Directive({
-  selector: '[appFrappe]'
+  selector: '[appChart]'
 })
 export class FrappeDirective {
 
   // Input decorator specifies props injectable in the directive via html attributes
   @Input() title: string;
   @Input() type: string;
-  @Input() data: any;
+  @Input() height: number;
+  // Use the same name as the directive to bind the datas directly on directive call
+  @Input('appChart') datas: any;
 
   // Result of the directive processing (to be output in the element)
   @Output() chart: EventEmitter<any> = new EventEmitter<any>();
@@ -21,6 +23,15 @@ export class FrappeDirective {
     private el: ElementRef
   ) {
     // Initialise default values if not specified into directive attributes
+    this.type = this.type || 'bar';
+    this.height = this.height || 250;
+  }
+
+  ngOnChanges {
+    const chart = new Chart({
+      parent: this.el.nativeElement
+    });
+    this.chart.emit(chart);
   }
 
 }
