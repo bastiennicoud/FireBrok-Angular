b498927a1bece065217d1883cf8b12f3f0e5d083
bastien <bastien.nicoud@gmail.com>
2019-03-19 10:27:20 +0100

    Add graph to the topic details page.

--- src/app/components/frappe-graph/frappe-graph.component.ts
+++ src/app/components/frappe-graph/frappe-graph.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input, OnInit, ViewChild } from '@angular/core';
-import { Chart } from 'frappe-charts';
-import { createViewChild } from '@angular/compiler/src/core';
+// import { Chart } from 'frappe-charts';
+declare var frappe: any;
 
 @Component({
   selector: 'app-frappe-graph',
@@ -13,25 +13,28 @@ export class FrappeGraphComponent implements OnInit {
   @Input() type: string;
   @Input() height: number;
   @Input() data: any;
+  @Input() colors: any;
 
   @ViewChild('frappe_chart') chartBox;
 
-  chart: Chart;
+  chart;
 
   constructor() {
     // Initialise default values if not specified into directive attributes
     this.type = this.type || 'bar';
     this.height = this.height || 250;
+    this.colors = this.colors || ['red'];
   }
 
   ngOnInit() {
     // Generate the frappe chart on init
-    this.chart = new Chart(
+    this.chart = new frappe.Chart(
       this.chartBox.nativeElement,
       {
         data: this.data,
         type: this.type,
-        height: this.height
+        height: this.height,
+        colors: this.colors
       });
   }
 
--- src/app/views/dashboard/automatons/topics-details/topics-details.component.ts
+++ src/app/views/dashboard/automatons/topics-details/topics-details.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { map, switchMap } from 'rxjs/operators';
+import { ActivatedRoute, ParamMap, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { AngularFirestore } from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-topics-details',
@@ -7,9 +11,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TopicsDetailsComponent implements OnInit {
 
-  constructor() { }
+  topic$: Observable<any>;
+  flux$: Observable<any>;
+
+  heatMapDatas: object;
+
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private firestore: AngularFirestore
+  ) {
+    // Prepare graph dates
+    this.heatMapDatas = {
+      labels: ['tutu', 'toto'],
+      datasets: [
+        { values: [10, 30] }
+      ]
+    };
+  }
 
   ngOnInit() {
+    // Get topic details observable
+    this.topic$ = this.route.paramMap.pipe(
+      switchMap((params: ParamMap) => {
+        return this.firestore
+          .doc(`/automatons/${params.get('automaton_id')}/topics/${params.get('topic_id')}`)
+          .valueChanges().pipe(
+            map(e => {
+              return { id: params.get('id'), ...e };
+            })
+        );
+      })
+    );
+    // Get topic flux
+    this.flux$ = this.route.paramMap.pipe(
+      switchMap((params: ParamMap) => {
+        return this.firestore
+          .collection(`/automatons/${params.get('automaton_id')}/topics/${params.get('topic_id')}/flux`)
+          .valueChanges();
+      })
+    );
   }
 
 }
