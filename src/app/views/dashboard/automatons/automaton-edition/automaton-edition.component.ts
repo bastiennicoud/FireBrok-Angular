17c7ece7a8cbd308c27dbda5594ce46389bb8ae7
bastien <bastien.nicoud@gmail.com>
2019-03-27 08:58:35 +0100

    Add new component for the automaton edition page.

---
+++ src/app/views/dashboard/automatons/automaton-edition/automaton-edition.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-automaton-edition',
+  templateUrl: './automaton-edition.component.html',
+  styleUrls: ['./automaton-edition.component.less']
+})
+export class AutomatonEditionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
