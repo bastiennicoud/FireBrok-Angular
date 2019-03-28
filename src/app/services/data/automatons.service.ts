b4d6718b00948ffea840b610fa8ef21adb0efe92
bastien <bastien.nicoud@gmail.com>
2019-03-28 17:18:56 +0100

    Move automatons firebase references to service. The service is registered in the module, so the automaton observer is registered only on time for all the application lifecycle. (Before the reference is retrieved each time the component is displayed.

--- src/app/services/data/automatons.service.ts
+++ src/app/services/data/automatons.service.ts
@@ -1,9 +1,41 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AutomatonsService {
 
-  constructor() { }
+  automatons$: Observable<any>;
+  connectedAutomatons$: Observable<any>;
+
+  constructor(
+    private firestore: AngularFirestore
+  ) {
+    // Get the observable for automatons
+    this.automatons$ = firestore.collection(
+      'automatons'
+    ).snapshotChanges().pipe(
+      map(actions => actions.map(a => {
+        return {
+          id: a.payload.doc.id,
+          ...a.payload.doc.data()
+        };
+      }))
+    );
+    // Get the observable for connected automatons
+    this.connectedAutomatons$ = firestore.collection(
+      'automatons',
+      ref => ref.where('connected', '==', true)
+    ).snapshotChanges().pipe(
+      map(actions => actions.map(a => {
+        return {
+          id: a.payload.doc.id,
+          ...a.payload.doc.data()
+        };
+      }))
+    );
+  }
 }
